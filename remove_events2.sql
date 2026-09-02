e43dd7826f8f370295ef0508070c14bca356cbda
Hadeel Ibrahim <po24432@bristol.ac.uk>
2026-09-02 02:32:30 +0100

    Remove temporary event data-cleanup SQL scripts
    These were one-off scripts (cleanup_events.sql, add_events.sql,
    remove_events2.sql) used to update demo event data directly in the
    local database. Not needed in the repo going forward.

--- remove_events2.sql
+++
@@ -1,9 +0,0 @@
--- Eventify: remove 2 more events.
--- Run from your Mac terminal:
---   cd /Users/hadeel/event-management-platform
---   mysql -u root eventify < remove_events2.sql
-
-DELETE FROM events WHERE title IN (
-  'AI & Robotics Tech Meetup',
-  'Downtown Business Networking Night'
-);
